eeee2a1f0f8203c6d76252dc51db858e0ce9cb46
Paweł <pawel.pawlaszczyk@gmail.com>
2024-10-18 20:20:26 +0200

    feat: New page for with details for user

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,24 +8,25 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+// implements OnInit 
+export class HomeComponent {
   registerMode = false;
-  users: any;
-  http = inject(HttpClient);
+  // users: any;
+  // http = inject(HttpClient);
 
   registerToggle(){
     this.registerMode =  !this.registerMode
   }
-ngOnInit(): void {
-  this.getUsers();
-}
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed'),
-    })
-  }
+// ngOnInit(): void {
+//   this.getUsers();
+// }
+//   getUsers(){
+//     this.http.get('https://localhost:5001/api/users').subscribe({
+//       next: response => this.users = response,
+//       error: error => console.log(error),
+//       complete: () => console.log('Request has completed'),
+//     })
+//   }
   cancelRegisterMode(event:boolean){
     this.registerMode = event;
   }
